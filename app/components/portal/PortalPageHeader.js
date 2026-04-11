b1e7556cf7cc376d0f0ed27b6aec2e6760fe24e8
latz09 <85132328+latz09@users.noreply.github.com>
2026-04-11 14:34:58 -0500

    fafa

--- app/components/portal/PortalPageHeader.js
+++ app/components/portal/PortalPageHeader.js
@@ -10,7 +10,7 @@ export default function PortalPageHeader({ variant, label, title }) {
     <div className="mb-12">
      
       <h1 className="text-3xl lg:text-4xl opacity-70 font-semibold">{title}</h1>
-       <p className={`font-mono text-xs lg:text-sm ${color} opacity-75 tracking-widest uppercase mt-2 pl-0.5`}>
+       <p className={`font-mono text-xs lg:text-sm ${color} opacity-75 tracking-widest uppercase mt-1 lg:mt-2 pl-0.5`}>
         {label}
       </p>
     </div>
